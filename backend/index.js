76fa48bbd947cef7f86f7b074873ed93bd758799
Dhanushika Dharmasena <dhanushikadharmasena456@gmail.com>
2025-02-19 12:06:15 +0530

    updated commit

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,23 @@
-import express, { request, response } from "express";
+import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-import { Book } from "./models/bookModel.js";
 import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
 //Middleware for parsing request body
 app.use(express.json());
+
+
+//Middleware for handling CORS policy
+//Allow custom Origins
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type']
+}));
+
 
 app.get("/", (request, response) => {
   console.log(request);
