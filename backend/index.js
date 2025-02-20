f10ac785dd41bd5436b8bf785ef020602929771f
Dhanushika Dharmasena <dhanushikadharmasena456@gmail.com>
2025-02-20 09:53:07 +0530

    Home page and routes for other pages commit

--- backend/index.js
+++ backend/index.js
@@ -9,22 +9,23 @@ const app = express();
 //Middleware for parsing request body
 app.use(express.json());
 
-
 //Middleware for handling CORS policy
+app.use(cors());
 //Allow custom Origins
-app.use(cors({
-  origin: 'http://localhost:3000',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type']
-}));
-
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
 
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("Welcome to MERN Stack");
 });
 
-app.use('/books', booksRoute);
+app.use("/books", booksRoute);
 
 mongoose
   .connect(mongoDBURL)
