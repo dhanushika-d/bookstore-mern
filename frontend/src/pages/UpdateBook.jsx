9d13bf5bd6eca60e05d690e641512b687e3983c3
Dhanushika Dharmasena <dhanushikadharmasena456@gmail.com>
2025-02-20 14:55:43 +0530

    alert with notistack commit

--- frontend/src/pages/UpdateBook.jsx
+++ frontend/src/pages/UpdateBook.jsx
@@ -3,6 +3,7 @@ import BackButton from '../components/BackButton'
 import Spinner from '../components/Spinner'
 import axios from 'axios'
 import { useNavigate, useParams } from 'react-router-dom'
+import { useSnackbar } from 'notistack'
 
 const UpdateBook = () => {
   const [title, setTitle] = useState('');
@@ -11,6 +12,7 @@ const UpdateBook = () => {
   const [loading, setLoading] =useState(false);
   const navigate = useNavigate();
   const { id } = useParams();
+  const {enqueueSnackbar} = useSnackbar();
 
   useEffect(() => {
     setLoading(true);
@@ -38,11 +40,13 @@ const UpdateBook = () => {
       .put(`http://localhost:5555/books/${id}`, data)
       .then(() => {
         setLoading(false);
+        enqueueSnackbar('Book Updated Succesfully', {variant: 'success'});
         navigate('/');
       })
       .catch((error) => {
         setLoading(false);
-        alert('An error happened. Please Check Console');
+        // alert('An error happened. Please Check Console');
+        enqueueSnackbar('Error', {variant: 'error'});
         console.log(error);
       });
   }
