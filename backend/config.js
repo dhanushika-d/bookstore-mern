d89af74d13ae1df9fe7dd9839006973e2af51ff3
Dhanushika Dharmasena <dhanushikadharmasena456@gmail.com>
2025-02-21 09:53:34 +0530

    Removed config.js from tracking

--- backend/config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL = 'mongodb+srv://root:dhanushika@bookstore-mern.is0xt.mongodb.net/books-collection?retryWrites=true&w=majority&appName=bookstore-mern';
